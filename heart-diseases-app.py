2dabc02fa11935c67d08485640d8436654454ad2
julia semenuk <julia.semenuk@gmail.com>
2023-03-03 22:26:37 +0300

    special edition for user app (without custom transformers)

--- heart-diseases-app.py
+++ heart-diseases-app.py
@@ -5,19 +5,14 @@
 
 import streamlit as st
 import pandas as pd
-import numpy as np
 import time
 import os
 
 from joblib import load
 
-#%% CUSTOM TRANSFORMERS USED BY MODEL
-
-from custom_transformers import age_to_year,BMITransformer,APTransformer
-
 #%% LOAD MODEL
 
-model = load(os.path.dirname(__file__) + '/model.joblib')
+model = load(os.path.dirname(__file__) + '/heart_diseases_model.joblib')
 
 #%% HEADER
 
@@ -35,16 +30,16 @@ with st.container():
 
 gender_select = st.radio('Пол',('Мужчина','Женщина'),label_visibility='hidden')
 
-age_year = st.slider('Возраст', min_value=30, max_value=100)
-height = st.slider('Рост', min_value=100, max_value=220)
-weight = st.slider('Вес', min_value=40, max_value=200)
+age_year = st.slider('Возраст', min_value=30, max_value=80)
+height = st.slider('Рост', min_value=120, max_value=220)
+weight = st.slider('Вес', min_value=40, max_value=170)
 
 st.subheader('Давление')
 ap1,ap2 = st.columns(2,gap='large')
 with ap1:
-    ap_hi = st.slider('верхнее', min_value=60, max_value=220)
+    ap_hi = st.slider('верхнее', min_value=80, max_value=220)
 with ap2:
-    ap_lo = st.slider('нижнее', min_value=40, max_value=200)
+    ap_lo = st.slider('нижнее', min_value=60, max_value=180)
 
 st.subheader('Биохимия')
 bl1,bl2 = st.columns(2,gap='large')
@@ -71,11 +66,13 @@ age = age_year *365
 smoke = 0 if smoke_select=='Не курю' else 1
 alco = 0 if alco_select=='не употребляю' else 1
 active = 0 if active_select=='малоподвижный' else 1
+bmi = weight/(height**2)*10000
 
 risk_data = pd.DataFrame({'age'   : [age],
                           'gender': [gender],
                           'height': [height],
                           'weight': [weight],
+                          'bmi'   : [bmi],
                           'ap_hi' : [ap_hi],
                           'ap_lo' : [ap_lo],
                           'cholesterol': [cholesterol],
@@ -92,39 +89,41 @@ risk_percent = risk * 100
 
 #%% FEATURE IMPORTANCES
 
-to_scale  = ['age','weight','bmi','ap_hi','ap_lo','cholesterol','gluc']
-to_encode = ['gender','smoke','alco','active']
-cat_names = model['pre'].steps[1][1].named_transformers_['cat'].get_feature_names_out()
-num_names = model['pre'].steps[1][1].named_transformers_['num'].get_feature_names_out(to_scale)
-all_names = np.concatenate([cat_names, num_names])
-feature_importances = pd.DataFrame({'features':all_names,
+feature_names = model['pre'].get_feature_names_out()
+feature_importances = pd.DataFrame({'features':feature_names,
                                     'feature_importances':model._final_estimator.feature_importances_})
 
-# SUM importances WEIGHT+BMI and AP_HI+AP_LO to common variables weight and ap
+# SUM importances AP_HI+AP_LO to single variable AP
 # (optimize the number of output variables)
 
-feature_importances.loc[len(feature_importances)] = {'features':'common_weight',
-                                                     'feature_importances':feature_importances[feature_importances['features'].isin(['weight','bmi'])]['feature_importances'].sum()}
-feature_importances.loc[len(feature_importances)] = {'features':'common_ap',
-                                                     'feature_importances':feature_importances[feature_importances['features'].isin(['ap_hi','ap_lo'])]['feature_importances'].sum()}
+feature_importances.loc[len(feature_importances)] = {'features':'ap',
+                                                     'feature_importances':feature_importances[feature_importances['features']\
+                                                                                               .isin(['num__ap_hi',
+                                                                                                      'num__ap_lo'])]\
+                                                         ['feature_importances'].sum()}
+feature_importances.loc[len(feature_importances)] = {'features':'w',
+                                                     'feature_importances':feature_importances[feature_importances['features']\
+                                                                                               .isin(['num__bmi',
+                                                                                                      'num__weight'])]\
+                                                         ['feature_importances'].sum()}
 
 # select and prepare features to output
 
-features_to_output = feature_importances[feature_importances['features'].isin(['common_weight',
-                                                                               'common_ap',
-                                                                               'cholesterol',
-                                                                               'gluc',
-                                                                               'smoke_1',
-                                                                               'alco_1',
-                                                                               'active_1'
+features_to_output = feature_importances[feature_importances['features'].isin(['ap',
+                                                                               'w',
+                                                                               'num__cholesterol',
+                                                                               'num__gluc',
+                                                                               'cat__smoke_1',
+                                                                               'cat__alco_1',
+                                                                               'cat__active_1'
                                                                                ])].copy()
-to_replace  = {'common_weight':'Лишний вес',
-               'common_ap':'Давление',
-               'cholesterol':'Холестерин',
-               'gluc':'Глюкоза',
-               'smoke_1':'Курение',
-               'alco_1':'Алкоголь',
-               'active_1':'Образ жизни'}
+to_replace  = {'w':'Лишний вес',
+               'ap':'Давление',
+               'num__cholesterol':'Холестерин',
+               'num__gluc':'Глюкоза',
+               'cat__smoke_1':'Курение',
+               'cat__alco_1':'Алкоголь',
+               'cat__active_1':'Образ жизни'}
 features_to_output['features'] = features_to_output['features'].replace(to_replace)
 features_to_output['feature_importances'] = features_to_output['feature_importances'] * risk_percent
 features_to_output = features_to_output.sort_values(by='feature_importances',ascending=False)
